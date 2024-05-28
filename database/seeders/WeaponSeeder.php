d0132e9a1490d1d6f746d2e2bf6a5693145d096f
giovanni2299 <giovanni-sorrentino@hotmail.it>
2024-05-28 17:24:24 +0200

    ADD: seader and cvs

---
+++ database/seeders/WeaponSeeder.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Weapon;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Validation\Rules\Exists;
+
+class WeaponSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+
+    
+    public function run(){
+        DB::table('weapons')->truncate();
+        $data = $this->getCSVData( __DIR__ . '/items.csv');
+
+        foreach($data as $index => $row){
+            if($index > 0){
+                $new_weapon = new Weapon();
+                $new_weapon->name = $row[0];
+                $new_weapon->category = $row[3];
+                $new_weapon->weight = $row[4];
+                $new_weapon->cost = $row[5];
+                $new_weapon->damage_dice = $row[6];
+                $new_weapon->save();
+
+            }
+        }
+    }
+
+    public function getCSVData(string $itemPath)
+    {
+        $data = [];
+
+        $file_stream = fopen($itemPath, 'r') ;
+
+        if($file_stream === false){
+            exit('cannot open the file'. $itemPath);
+        }
+          //interrompe il ciclo solo se la funzione cvs restituisce false (la funzione csv restituisce fals solo quando non ci sono più righe da leggere)
+        while(($row = fgetcsv($file_stream)) !== false ){
+            $data[] = $row;
+        }
+
+        fclose($file_stream);
+
+        return $data;
+    }
+}
