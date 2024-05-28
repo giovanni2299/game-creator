7f6e252a8b6ab41fddbdb8af8bc6a6ca53abaca0
giovanni2299 <giovanni-sorrentino@hotmail.it>
2024-05-28 16:34:49 +0200

    ADD: migrate table_wepons

---
+++ database/migrations/2024_05_28_140229_create_weapons_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('weapons', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',50);
+            $table->string('category',50);
+            $table->string('weight',10);
+            $table->string('cost',12);
+            $table->string('damage_dice',5);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('weapons');
+    }
+};
