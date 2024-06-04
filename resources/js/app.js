8d28891b695e3e9ce9f6768b318d4279d9b603d3
giovanni2299 <giovanni-sorrentino@hotmail.it>
2024-06-04 15:55:13 +0200

    ADD: all the stuff

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,7 @@
-import '~resources/scss/app.scss'
-import * as bootstrap from 'bootstrap';
 import './bootstrap';
+import '~resources/scss/app.scss';
+import '~icons/bootstrap-icons.scss';
+import * as bootstrap from 'bootstrap';
+import.meta.glob([
+    '../img/**'
+])
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
-use App\Models\Weapon;
 use App\Http\Controllers\CharacterController;
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\WeaponController;
 use Illuminate\Support\Facades\Route;
 
@@ -15,23 +15,13 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
 Route::get('/', function () {
 
    
     return view('welcome');
 })->name('welcome');
 
-//WEAPONS
 
-//INDEX
-Route::get('/weapons', [WeaponController::class, 'index'])->name('weapons.index');
-//CREATE
-Route::get('/weapons/create', [WeaponController::class, 'create'])->name('weapons.create');
-//SHOW
-Route::get('/weapons/{weapon}', [WeaponController::class, 'show'])->name('weapons.show');
-//STORE
-Route::get('/weapons',[WeaponController::class, 'store'])->name('weapons.store');
 
 
 
@@ -40,18 +30,47 @@ Route::get('/weapons',[WeaponController::class, 'store'])->name('weapons.store')
 
 
 //CHARACTERS//
+Route::middleware('auth')->group(function () {
+    //WEAPONS
 
-// INDEX
-Route::get('/characters', [CharacterController::class, 'index'])->name('characters.index');
-// CREATE
-Route::get('/characters/create', [CharacterController::class, 'create'])->name('characters.create');
-// SHOW
-Route::get('/characters/{character}', [CharacterController::class, 'show'])->name('characters.show');
-// STORE
-Route::post('/characters', [CharacterController::class, 'store'])->name('characters.store');
-// EDIT
-Route::get('/characters/{character}/edit', [CharacterController::class, 'edit'])->name('characters.edit');
-// UPDATE
-Route::put('/characters/{character}', [CharacterController::class, 'update'])->name('characters.update');
-// DESTROY
-Route::delete('/characters/{character}', [CharacterController::class, 'destroy'])->name('characters.destroy');
+    //INDEX
+    Route::get('/weapons', [WeaponController::class, 'index'])->name('weapons.index');
+    //CREATE
+    Route::get('/weapons/create', [WeaponController::class, 'create'])->name('weapons.create');
+    //SHOW
+    Route::get('/weapons/{weapon}', [WeaponController::class, 'show'])->name('weapons.show');
+    //STORE
+    Route::get('/weapons',[WeaponController::class, 'store'])->name('weapons.store');
+
+
+    // INDEX
+    Route::get('/characters', [CharacterController::class, 'index'])->name('characters.index');
+    // CREATE
+    Route::get('/characters/create', [CharacterController::class, 'create'])->name('characters.create');
+    // SHOW
+    Route::get('/characters/{character}', [CharacterController::class, 'show'])->name('characters.show');
+    // STORE
+    Route::post('/characters', [CharacterController::class, 'store'])->name('characters.store');
+    // EDIT
+    Route::get('/characters/{character}/edit', [CharacterController::class, 'edit'])->name('characters.edit');
+    // UPDATE
+    Route::put('/characters/{character}', [CharacterController::class, 'update'])->name('characters.update');
+    // DESTROY
+    Route::delete('/characters/{character}', [CharacterController::class, 'destroy'])->name('characters.destroy');
+});
+
+
+
+
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
