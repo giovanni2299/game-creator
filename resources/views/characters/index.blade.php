5b4bda827f4e2df26602342105797dcb4b841f81
RoccoCerro <rcerro02@gmail.com>
2024-06-14 17:37:46 +0200

    Add: style index characters

--- resources/views/characters/index.blade.php
+++ resources/views/characters/index.blade.php
@@ -3,39 +3,45 @@
 @section('content')
 
 <section>
-    <div class="container p-3" >
-        <div class="ms-3">
-            <a class="btn btn-success" href="{{route('welcome')}}">Go to Welcome</a>
-            <a class="btn btn-primary " href="{{route('characters.create')}}" >
-                New Character
-            </a>
+    <section class="hero ">
+        <img src="/img/dragon.jpeg" alt="">
+        <div class="container">
 
         </div>
-        <div class="row gx-2 gy-2 p-3">
-            @foreach($characters as $character)
-            <div class="col-3">
-                <div class="card">
-                    <div class="card-header text-center">
-                        <p>
-                            {{$character->name}}
+    </section>
+    <section class="page-main">
+
+        <div class="container p-3" >
+            
+            <div class="button-new-character">
+                <a class="btn btn-dark " href="{{route('characters.create')}}" >
+                    New Character
+                </a>
     
-                        </p>
-                    </div>
-                    <div class="card-body d-flex justify-content-center">
-                        <p>
-                            <a class="btn btn-primary" href="{{route('characters.show', $character)}}">Show Details</a>
-        
-                        </p>
-
-                    </div>
-                   
-
-                </div>
-                
             </div>
-            @endforeach
+            <div class="row pt-3 ">
+                @foreach($characters as $character)
+                <div class="col-3 p-2">
+                    <div class="card my-card text-bg-secondary h-100 ">
+                        <div class="card-header my-card-header text-center">
+                            <p>
+                                {{$character->name}}
+        
+                            </p>
+                        </div>
+                        <div class="card-body d-flex justify-content-center align-items-end">
+                                <a class="btn btn-outline-dark" href="{{route('characters.show', $character)}}">Show Details</a>
+        
+                        </div>
+                       
+    
+                    </div>
+                    
+                </div>
+                @endforeach
+            </div>
         </div>
-    </div>
+    </section>
 </section>
 
 @endsection
